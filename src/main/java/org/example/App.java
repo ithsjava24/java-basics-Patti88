be857ca3cf1594bd2c0337fcb2534de595012046
PatriciaNilsson <patricia.nilsson@iths.se>
2024-10-02 15:46:45 +0200

    First commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,10 @@
 package org.example;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.of("SE"));
         Scanner scanner = new Scanner(System.in);
         double[] priser = new double[0]; // lagra dem inmatade priser
 
